2cf0666e67e4a4ef85aa8fcc5318e957c41b9641
andyphilbertjr <andyphilbertjr@gmail.com>
2019-02-05 02:52:23 -0500

    fetch character data added

--- src/App.js
+++ src/App.js
@@ -2,33 +2,48 @@ import React, { Component } from 'react';
 import './App.css';
 const charactersData = require('./characters.json')
 
-function getCharacterInfo(url){
-  return fetch(url)
-        .then( response => response.json)
-        .then(data => JSON.stringify(data))
-}
 
-const Characters = function(){
-  return charactersData.characters.map( stars=>{
-    let data = getCharacterInfo(`${stars.url}`)
-    let name = data
-    console.log(name)
-    return (
-      <div>
-        {stars.name}
-      </div>
-    )
-  })
-}
+function Characters(props){
+  function getData(){
+    return fetch(props.url)
+            .then( response => response.json() )
+            .then( data => document.getElementById(props.name)
+                            .innerHTML = `<div>${JSON.stringify(data.films)}</div>`)
+  }
+
+  return (
+          <div>
+            <div className='characterCards'>
+              {props.name}
+              <button onClick={getData}>Pick Character</button>
+            </div>
+            <div id={props.name} className='characterDetails'>
+            </div>
+          </div>
+
+          )
+} 
 
 
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.getCharacters = this.getCharacters.bind(this)
+   // this.getData = this.getData.bind(this)
+  }
+  
+  getCharacters(){
+    return charactersData.characters.map( (item , key) => {
+
+      return <Characters key={item.name} name={item.name} url={item.url}/>
+    }) 
+    }
 
   render() {
     return (
       <div>
-        <Characters />
+        {this.getCharacters()}
       </div>
     );
   }
